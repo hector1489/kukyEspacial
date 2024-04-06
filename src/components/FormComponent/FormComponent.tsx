3c4245c7f1f04ea0e121f348a90aae0088722c05
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-04-06 16:20:27 -0300

    [fix] flex-form

--- src/components/FormComponent/FormComponent.tsx
+++ src/components/FormComponent/FormComponent.tsx
@@ -3,16 +3,16 @@
 const FormComponent = () => {
 
   return (
-    <form className="m-2">
-      <label className="input input-bordered flex items-center gap-2">
-        Name
-        <input type="text" className="grow" placeholder="Daisy" />
+    <form className="m-2 w-2/4">
+      <label className="input input-bordered flex items-center gap-2 mt-2">
+        Name <span className="text-success font-bold"> :</span>
+        <input type="text" className="grow" placeholder="your Name" />
       </label>
-      <label className="input input-bordered flex items-center gap-2">
-        Email
-        <input type="text" className="grow" placeholder="daisy@site.com" />
+      <label className="input input-bordered flex items-center gap-2 mt-2">
+        Email <span className="text-success font-bold"> :</span>
+        <input type="text" className="grow" placeholder="XXXXX@site.com" />
       </label>
-      <textarea className="textarea textarea-secondary" placeholder="Bio"></textarea>
+      <textarea className="textarea textarea-secondary mt-2" placeholder="your comment"></textarea>
 
     </form>
   )
--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -3,12 +3,14 @@ import FormComponent from "../../components/FormComponent/FormComponent"
 
 const Contact = () => {
 
-    return(
-        <div className="text-center">
-        <h3 className="bg-base-300">Contacto <span className="text-success font-bold">:</span></h3>
+  return (
+    <div className="text-center">
+      <h3 className="bg-base-300">Contacto <span className="text-success font-bold">:</span></h3>
+      <div className="flex items-center justify-center">
         <FormComponent />
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Contact
