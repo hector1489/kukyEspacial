6533bbd2e006b0b994212aab9529d9ff3665dc2c
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-12-01 09:48:33 -0300

    add buttons social icons

--- src/views/Contact/Contact.tsx
+++ src/views/Contact/Contact.tsx
@@ -9,8 +9,20 @@ const Contact = () => {
 
       <div className="contact-form-container">
         <h3 className="bg-base-300">Contacto <span className="text-success font-bold">:</span></h3>
-          <FormComponent />
+        <FormComponent />
       </div>
+      <div className="contact-view-buttons">
+        <a href="" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-square-facebook"></i>
+        </a>
+        <a href="https://www.instagram.com/rashtistore/profilecard/" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-instagram"></i>
+        </a>
+        <a href="https://wa.me/54270328" target="_blank" rel="noopener noreferrer">
+          <i className="fa-brands fa-whatsapp"></i>
+        </a>
+      </div>
+
 
     </div>
   )
