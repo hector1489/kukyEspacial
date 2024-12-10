5ce43556775ecfe6cb8633081d29b653a85af9ff
Hector Gonzalez <g.p.hector.alejandro@gmail.com>
2024-12-10 11:52:52 -0300

    fix data json url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from 'tailwindcss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://hector1489.github.io/kukyEspacial",
   css: {
     postcss: {
       plugins: [tailwindcss()],
